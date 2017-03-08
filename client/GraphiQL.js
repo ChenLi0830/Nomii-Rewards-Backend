b9264928bfb3bb5880919fd2c676450c779c015b
Chen Li <yichen.li0830@gmail.com>
2017-03-08 08:15:19 -0800

    Fix client graphQL link

--- client/GraphiQL.js
+++ client/GraphiQL.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import GraphiQL from 'graphiql';
 import fetch from 'isomorphic-fetch';
 
-// const link = "https://uo1lwdfvpg.execute-api.us-west-2.amazonaws.com/dev";
-const link = "http://localhost:4000/graphql";
+const link = "https://uo1lwdfvpg.execute-api.us-west-2.amazonaws.com/dev";
 
 function graphQLFetcher(graphQLParams) {
   return fetch(link + '/graphql', {
