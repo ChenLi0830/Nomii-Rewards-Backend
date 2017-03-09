a65254589809a9393ce5ed99a0bd63b235dcc9de
Chen Li <yichen.li0830@gmail.com>
2017-03-09 12:18:55 -0800

    Move methods into jsonDB folder

--- serverless/graphql/schema.js
+++ serverless/graphql/schema.js
@@ -3,7 +3,7 @@
 require('isomorphic-fetch');
 
 const _ = require('lodash');
-const db = require('./database');
+const db = require('./jsonDB');
 const graphql = require('graphql'),
     GraphQLObjectType = graphql.GraphQLObjectType,
     GraphQLString = graphql.GraphQLString,
