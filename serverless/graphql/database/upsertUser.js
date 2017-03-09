f7dac9c6784bf35122512217542e36b7478bbf0c
Chen Li <yichen.li0830@gmail.com>
2017-03-08 23:19:30 -0800

    Update upsertUser method

--- serverless/graphql/database/upsertUser.js
+++ serverless/graphql/database/upsertUser.js
@@ -1,21 +1,31 @@
+'use strict';
+const getUser = require('./getUser');
 
 const upsertUser = (id, fbName) => {
-  return fetch(`http://localhost:3000/users`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      id,
-      fbName,
-      cards: [],
-    })
-  })
+  return fetch(`http://localhost:3000/users/${id}`)
       .then((response) => {
         if (response.status >= 400) {
-          throw new Error("Bad response from server");
+          return fetch(`http://localhost:3000/users`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              id,
+              fbName,
+              cards: [],
+            })
+          })
+              .then((response) => {
+                if (response.status >= 400) {
+                  throw new Error("Bad response from server");
+                }
+                return response.json();
+              })
         }
-        return response.json();
+        else {
+          return response.json();
+        }
       })
 };
 
