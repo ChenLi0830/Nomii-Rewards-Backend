1126566af4570741c847a348fbfc0302a6a529f6
Chen Li <yichen.li0830@gmail.com>
2017-03-13 23:56:47 -0700

    Update upsertUser method

--- serverless/graphql/database/upsertUser.js
+++ serverless/graphql/database/upsertUser.js
@@ -2,6 +2,7 @@
 const UserTable = require('./config').UserTable;
 let AWS = require('./config').AWS;
 let docClient = new AWS.DynamoDB.DocumentClient();
+const api = require('../api');
 
 const getUserFromDB = (id)=>{
   let params = {
@@ -14,35 +15,55 @@ const getUserFromDB = (id)=>{
   return new Promise((resolve, reject) => {
     docClient.get(params, (err, data) => {
       if (err) {
-        console.error("Unable to get item. Error JSON:", JSON.stringify(err), err.stack);
+        console.error("Unable to get user. Error JSON:", JSON.stringify(err), err.stack);
         return reject(err);
       }
       let user = data.Item;
       console.log("user", user);
-      if (!user) user = {};
       // console.log("GetItem succeeded:", JSON.stringify(item));
       resolve(user);
     });
   })
 };
 
-const insertUserToDB = (id, fbName) => {
-  const user = {
-    id,
-    fbName,
-    cards: [],
-  };
+const insertUserToDB = (user) => {
   return new Promise((resolve, reject) => {
     let params = {
       TableName: UserTable,
       Item: user,
+      // ReturnConsumedCapacity: "TOTAL",
     };
     docClient.put(params, (err, data) => {
       if (err) {
-        console.error("Unable to add item. Error JSON:", JSON.stringify(err), err.stack);
+        console.error("Unable to insert user. Error JSON:", JSON.stringify(err), err.stack);
         return reject(err);
       } else {
-        console.log("User added successfully");
+        console.log("User inserted successfully");
+        resolve(user);
+      }
+    });
+  })
+};
+
+const updateUserInDB = (user) => {
+  return new Promise((resolve, reject) => {
+    let params = {
+      TableName: UserTable,
+      Key: {
+        "id": user.id,
+      },
+      UpdateExpression: "SET lastLoginAt = :time",
+      ExpressionAttributeValues: {
+        ":time": api.getTimeInSec(),
+      },
+      ReturnValues: "ALL_NEW",
+    };
+    docClient.update(params, (err, data) => {
+      if (err) {
+        console.error("Unable to update user. Error JSON:", JSON.stringify(err), err.stack);
+        return reject(err);
+      } else {
+        console.log("User updated successfully");
         resolve(user);
       }
     });
@@ -52,9 +73,28 @@ const insertUserToDB = (id, fbName) => {
 const upsertUser = (id, fbName) => {
   return getUserFromDB(id)
       .then(user => {
-        // return user;
-        if (user && user.id) return user;
-        else return insertUserToDB(id, fbName);
+        //Create user
+        if (!user) {
+          user = {
+            id,
+            fbName,
+            registeredAt: api.getTimeInSec(),
+            lastLoginAt: api.getTimeInSec(),
+            cards: [],
+            usedCards: [],
+            redeemedCoupons: [],
+            visitedRestaurants: [],
+            ownedRestaurants: [],
+          };
+          console.log("insertUserToDB", user);
+          return insertUserToDB(user);
+        }
+        // Update user
+        else {
+          user.lastLoginAt = api.getTimeInSec();
+          console.log("updateUserInDB", user);
+          return updateUserInDB(user);
+        }
       });
 };
 
