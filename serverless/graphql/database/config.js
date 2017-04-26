373a11be0ddd12369aecb0ec26bd61e44253f974
Chen Li <yichen.li0830@gmail.com>
2017-04-26 13:33:19 -0700

    Setting AWS configurations - use environment variable to differentiate local express server from AWS lambda

--- serverless/graphql/database/config.js
+++ serverless/graphql/database/config.js
@@ -5,12 +5,15 @@ let AWS = require("aws-sdk");
 /**
  * For testing on local DynamoDB
 **/
-// AWS.config.update({
-//   region: "us-west-2",
-//   endpoint: "http://localhost:8000",
-//   accessKeyId: "123",
-//   secretAccessKey: "345",
-// });
+console.log("process.env.DEBUG_MODE", process.env.DEBUG_MODE);
+if (process.env.DEBUG_MODE){
+  AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000",
+    accessKeyId: "123",
+    secretAccessKey: "345",
+  });
+}
 
 const UserTable = "Nomii-Rewards-Users";
 const RestaurantTable = "Nomii-Rewards-Restaurants";
