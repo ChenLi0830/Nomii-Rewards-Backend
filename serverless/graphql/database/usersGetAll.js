4df9a376d3485f49d097e71c23623b23cf7e9030
Chen Li <yichen.li0830@gmail.com>
2017-04-30 17:51:36 -0700

    Taking into account the 1M scan limit of DynamoDB and implement re-scanning when LastEvaluatedKey != null

--- serverless/graphql/database/usersGetAll.js
+++ serverless/graphql/database/usersGetAll.js
@@ -5,21 +5,54 @@ let docClient = new AWS.DynamoDB.DocumentClient();
 
 /**
  * Get all users in the UserTable
+ * @param {Object} options - The options for the scan method.
  * */
-const getAllUsers = () => {
-  const params = {
+const getAllUsers = (options) => {
+  let ExpressionAttributeNames,
+      newProjectionExpression;
+  
+  if (options){
+    const {projectionExpression} = options;
+    if (projectionExpression){
+      ExpressionAttributeNames = {};
+      for (let attributeName of projectionExpression.replace(/ /g,'').split(',')){
+        ExpressionAttributeNames[`#${attributeName}`] = attributeName;
+      }
+      newProjectionExpression = Object.keys(ExpressionAttributeNames).join(',');
+    }
+  }
+  
+  let params = {
     TableName: UserTable,
+    ProjectionExpression: newProjectionExpression,
+    ExpressionAttributeNames,
+    // Limit: 2,
   };
   
   return new Promise((resolve, reject) => {
-    docClient.scan(params, (err, data) => {
+    let result = [];
+    
+    const onScan = (err, data) => {
       if (err) {
         console.error("Unable to get all users. Error JSON:", JSON.stringify(err), err.stack);
         return reject(err);
       }
-      let users = data.Items;
-      resolve(users);
-    });
+      console.log("Object.keys(data)", Object.keys(data));
+      console.log(`${data.Items.length} items fetched`);
+      
+      result = result.concat(data.Items);
+  
+      // If there is more to scan, then scan again
+      if (typeof data.LastEvaluatedKey != "undefined") {
+        console.log("Scanning for more...");
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        docClient.scan(params, onScan);
+      } else {
+        resolve(result);
+      }
+    };
+    
+    docClient.scan(params, onScan);
   });
 };
 
