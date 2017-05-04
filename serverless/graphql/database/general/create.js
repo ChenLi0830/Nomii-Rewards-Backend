c557167d87b55eb0921403bcdc21570554860ae4
Chen Li <yichen.li0830@gmail.com>
2017-05-04 11:17:54 -0700

    Implementing general create method

---
+++ serverless/graphql/database/general/create.js
@@ -0,0 +1,35 @@
+'use strict';
+let AWS = require('../config').AWS;
+let docClient = new AWS.DynamoDB.DocumentClient();
+const _ = require('lodash');
+const api = require('../../api');
+
+const create = (tableName, item, options={}) => {
+  const {ReturnValues = "NONE", ReturnConsumedCapacity="NONE"} = options;
+  
+  const timeStamp = api.getTimeInSec();
+  
+  item.createdAt = timeStamp;
+  item.updatedAt = timeStamp;
+  
+  let params = {
+    TableName: tableName,
+    Item: item,
+    ReturnConsumedCapacity: ReturnConsumedCapacity,
+    ReturnValues: ReturnValues,
+  };
+  
+  return new Promise((resolve, reject) => {
+    docClient.put(params, (err, data) => {
+      if (err) {
+        console.error(`Unable to insert item into ${tableName}. Error JSON:`, JSON.stringify(err), err.stack);
+        return reject(err);
+      } else {
+        console.log(`Successfully inserted item into table ${tableName}`);
+        resolve(item);
+      }
+    });
+  });
+};
+
+module.exports = create;
